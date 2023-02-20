f9886675bf4dbb4c0d4ec7d856d94c7bc5fb0483
fedor <f.shatruk@thesis.team>
2023-02-20 11:53:14 +0300

    remove Role

--- hire_hire/static_info/models/our_team.py
+++ hire_hire/static_info/models/our_team.py
@@ -3,14 +3,6 @@ from sorl.thumbnail import ImageField
 
 
 class OurTeam(models.Model):
-    class Role(models.IntegerChoices):
-        GURU = 0, 'гуру проекта'
-        PM = 1, 'менеджеры проекта'
-        FRONT_END = 2, 'фронтенд разработчики'
-        BACK_END = 3, 'бэкенд-разработчики'
-        QA = 4, 'QA-тестировщики'
-        UI_UX_DESIGNER = 5, 'UX/UI-дизайнеры'
-
     first_name = models.CharField(
         'имя',
         max_length=150,
