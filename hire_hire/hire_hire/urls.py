52d808161992709e098d9cdf2f298218b28c4dff
Badsnus <anton.098.bal@gmail.com>
2023-02-26 19:13:02 +0500

    minor fix x2

--- hire_hire/hire_hire/urls.py
+++ hire_hire/hire_hire/urls.py
@@ -12,8 +12,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
     import debug_toolbar
 
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
