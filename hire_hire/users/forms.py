3a846c2c9f7b6241453f9ad8ca920a374ac8f493
Юлия Орлова <zaikunets@yandex.ru>
2023-02-26 11:31:21 +0300

    fixed forms.py

--- hire_hire/users/forms.py
+++ hire_hire/users/forms.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import BaseUserCreationForm
 from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
 
 
-class CreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
+class CreationForm(BaseUserCreationForm):
+    class Meta:
         model = User
-        fields = ('first_name', 'username', 'email',)
+        fields = ('first_name', 'username', 'email')
