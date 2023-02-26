b72f46fdc6c7913074cf2aec3f790d4f7ed84ddb
Badsnus <anton.098.bal@gmail.com>
2023-02-26 18:18:18 +0500

    add contributors page

---
+++ hire_hire/contributors/managers.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class ContributorManager(models.Manager):
+    def get_contributors_with_contacts_and_roles(self):
+        """если тут делать Contributor.role.field.name то будет цирк импорт"""
+        return (
+            self.get_queryset()
+            .select_related('role')
+            .prefetch_related('contacts')
+            .order_by('role')
+        )
--- hire_hire/hire_hire/urls.py
+++ hire_hire/hire_hire/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -11,6 +12,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
     import debug_toolbar
 
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
