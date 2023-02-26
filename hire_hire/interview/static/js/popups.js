5f504a892863f6c5c0574c600a9c4c003e27d884
Badsnus <anton.098.bal@gmail.com>
2023-02-26 15:18:40 +0500

    add new static

--- hire_hire/interview/static/js/popups.js
+++ hire_hire/interview/static/js/popups.js
@@ -7,35 +7,33 @@ const registerPopup = page.querySelector('.popup_type_register');
 const popupCloseButtons = page.querySelectorAll('.popup__close-btn');
 const registerButton = authPopup.querySelector('.form__button_type_register');
 const revivePasswordButton = authPopup.querySelector('.form__button_type_forgot');
-const revivePasswordSubmitButton = revivePasswordPopup.querySelector('.form__button_type_revive-password');
 
 function closeAllPopups() {
-  page.classList.remove('page_disabled');
-  popups.forEach(popup => popup.classList.remove('popup_active'));
+    page.classList.remove('page_disabled');
+    popups.forEach(popup => popup.classList.remove('popup_active'));
 };
 
 popupCloseButtons.forEach(closeButton => closeButton.addEventListener('click', closeAllPopups));
 
 function openAuthPopup() {
-  page.classList.add('page_disabled');
-  authPopup.classList.add('popup_active');
+    page.classList.add('page_disabled');
+    authPopup.classList.add('popup_active');
 }
 
 function openRevivePasswordPopup() {
-  closeAllPopups();
-  page.classList.add('page_disabled');
-  revivePasswordPopup.classList.add('popup_active');
+    closeAllPopups();
+    page.classList.add('page_disabled');
+    revivePasswordPopup.classList.add('popup_active');
 }
 
 function openRegisterPopup() {
-  closeAllPopups();
-  page.classList.add('page_disabled');
-  registerPopup.classList.add('popup_active');
+    closeAllPopups();
+    page.classList.add('page_disabled');
+    registerPopup.classList.add('popup_active');
 }
 
 authButton.addEventListener('click', openAuthPopup);
 registerButton.addEventListener('click', openRegisterPopup);
-revivePasswordButton.addEventListener('click', openRevivePasswordPopup);
 
 // Revive Passwor Popup
 
@@ -45,16 +43,24 @@ const repeatPasswordLabel = revivePasswordPopup.querySelector('.form__label_type
 const infoText = revivePasswordPopup.querySelector('.form__info');
 
 function changeReviveFormInputs() {
-  if (emailLabel.classList.contains('form__label_type_visible')) {
-    emailLabel.classList.remove('form__label_type_visible');
-    passwordLabel.classList.add('form__label_type_visible');
-    repeatPasswordLabel.classList.add('form__label_type_visible');
-  } else {
-    emailLabel.classList.remove('form__label_type_visible');
-    passwordLabel.classList.remove('form__label_type_visible');
-    repeatPasswordLabel.classList.remove('form__label_type_visible');
-    infoText.classList.add('form__info_type_visible');
-  }
+    if (emailLabel.classList.contains('form__label_type_visible')) {
+        emailLabel.classList.remove('form__label_type_visible');
+        passwordLabel.classList.add('form__label_type_visible');
+        repeatPasswordLabel.classList.add('form__label_type_visible');
+    } else {
+        emailLabel.classList.remove('form__label_type_visible');
+        passwordLabel.classList.remove('form__label_type_visible');
+        repeatPasswordLabel.classList.remove('form__label_type_visible');
+        infoText.classList.add('form__info_type_visible');
+    }
 }
 
-revivePasswordSubmitButton.addEventListener('click', changeReviveFormInputs);
+popups.forEach((popup) => {
+    popup.addEventListener('click', (e) => {
+        if (e.target.classList.contains('popup_active')) {
+            closeAllPopups();
+        }
+        ;
+    });
+
+});
