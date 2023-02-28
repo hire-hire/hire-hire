4c6591129b2f45c2ad8cf8724f6ef360e2974322
Badsnus <anton.098.bal@gmail.com>
2023-02-28 16:51:41 +0500

    fix docstring

--- hire_hire/contributors/managers.py
+++ hire_hire/contributors/managers.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class ContributorManager(models.Manager):
     def get_contributors_with_contacts_and_roles(self):
-        """если тут делать Contributor.role.field.name то будет цирк импорт"""
+        """Если тут делать Contributor.role.field.name то будет цирк импорт."""
         return (
             self.get_queryset()
             .select_related('role')
