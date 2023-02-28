bcee21c9f1d11dacafd6b5c386e8d0ebe3eb14e8
Badsnus <anton.098.bal@gmail.com>
2023-02-28 16:54:51 +0500

    imports lexicographical and add slash

--- hire_hire/users/urls.py
+++ hire_hire/users/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 
 from users.views import (
-    SignUpView,
     CustomLoginView,
-    ProfileView,
     CustomLogoutView,
+    ProfileView,
+    SignUpView,
 )
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout', CustomLogoutView.as_view(), name='logout'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
 
     path('profile/', ProfileView.as_view(), name='profile'),
