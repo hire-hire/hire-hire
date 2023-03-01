e3784e19a0339e837b857c6fd8a74926d8284cca
Юлия Орлова <zaikunets@yandex.ru>
2023-03-01 20:45:11 +0300

    правки по ревью

--- hire_hire/duel/views.py
+++ hire_hire/duel/views.py
@@ -61,8 +61,8 @@ class DuelFlowQuestionView(DuelFlowGetDuelMixin, LoginRequiredMixin, FormView):
 
         context['player1'], context['player2'] = self.duel_players
         context['duel_question'] = self.duel.questions.get_no_answered()
-        context['total_questions'] = self.duel.questions.count()
-        context['current_question'] = (
+        context['total_questions_count'] = self.duel.questions.count()
+        context['current_question_number'] = (
             context['player1'].good_answers_count +
             context['player2'].good_answers_count +
             self.duel.wrong_answers_count +
