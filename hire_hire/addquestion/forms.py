397bb2f72a9d09cc72d40038492feeee6f54b749
Dmitrii Barilkin <dimabaril@gmail.com>
2023-03-09 19:04:18 +0100

    Добавил атрибуты CSS в форме. Дичь наверное.

--- hire_hire/addquestion/forms.py
+++ hire_hire/addquestion/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from .models import AddQuestion
+from interview.models import Language
 
 
 class AddQuestionForm(forms.ModelForm):
@@ -12,3 +13,26 @@ class AddQuestionForm(forms.ModelForm):
         labels = {'language': 'Язык',
                   'text': 'Ваш вопрос',
                   'answer': 'Ваш ответ', }
+
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'id': 'question',
+                'class': 'offer__textarea page__text',
+                'minlength': '10',
+                'maxlength': '500',
+                'placeholder': 'Введите вопрос',
+                'required': True,
+            }),
+            'answer': forms.Textarea(attrs={
+                'id': 'answer',
+                'class': 'offer__textarea page__text',
+                'minlength': '10',
+                'maxlength': '500',
+                'placeholder': 'Введите ответ',
+                'required': True,
+            }),
+            'language': forms.Select(attrs={'class': 'form-control'}),
+        }
+        initial = {
+            'language': Language.objects.get(title='Python'),
+        }
