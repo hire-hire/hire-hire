6c623b2da844a048f8ab51c6fe5dfa701d5f7605
Dmitrii Barilkin <dimabaril@gmail.com>
2023-03-09 17:12:44 +0100

    нужна помощь с фронтом. Куда пихать поля формы? сверху запилена форма и она работает. здесь же если пихаю в textarea ругается что не заполнены поля

---
+++ hire_hire/interview/static/js/duel.js
@@ -0,0 +1,7 @@
+const duelForm = document.querySelector('.duel__form');
+const duelInputsArr = duelForm.querySelectorAll('.duel__radio');
+const duelPlayersContainer = document.querySelector('.duel__players');
+
+duelPlayersContainer.style.gridTemplateColumns = "1fr 128px 1fr";
+duelPlayersContainer.style.gap = "32px";
+duelInputsArr[2].setAttribute('checked', 'true');
