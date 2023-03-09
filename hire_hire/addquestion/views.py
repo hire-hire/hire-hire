20c6f8c7d291edc4c53d96d2af964676d0cd4ebc
Dmitrii Barilkin <dimabaril@gmail.com>
2023-03-09 01:15:41 +0100

    добавил счетчик количества добавленных вопросов

--- hire_hire/addquestion/views.py
+++ hire_hire/addquestion/views.py
@@ -13,19 +13,25 @@ class AddQuestionView(CreateView):
     form_class = AddQuestionForm
     template_name = 'addquestion/addquestion.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        ago_24_hours = timezone.now() - timezone.timedelta(hours=24)
+        self.current_ip_address = request.META.get('REMOTE_ADDR')
+        self.addquestions_for24_count = (
+            AddQuestion.objects
+            .filter(Q(pub_date__gte=ago_24_hours) & Q(
+                ip_address=self.current_ip_address))).count()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['added_questions'] = self.addquestions_for24_count
+        return context
+
     def form_valid(self, form):
         form.instance.ip_address = self.request.META.get('REMOTE_ADDR')
         if self.request.user.is_authenticated:
             form.instance.author = self.request.user
-
-        ago_24_hours = timezone.now() - timezone.timedelta(hours=24)
-        addquestion_for24_count = (
-            AddQuestion.objects
-            .filter(Q(pub_date__gte=ago_24_hours) & Q(ip_address=form.instance
-                                                      .ip_address))).count()
-
-        if addquestion_for24_count >= MAX_ADDQUESTIONS_PER_DAY:
+        if self.addquestions_for24_count >= MAX_ADDQUESTIONS_PER_DAY:
             form.add_error(None, 'Вы исчерпали лимит вопросов на день.')
             return super().form_invalid(form)
-
         return super().form_valid(form)
