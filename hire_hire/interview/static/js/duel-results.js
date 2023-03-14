0f849651a816a5f2f72e80f4343fb52084060d28
Алексей Спиридонов <zayaczloy@yandex.ru>
2023-03-14 17:34:57 +0300

    fix: fixed duel-result.js

--- hire_hire/interview/static/js/duel-results.js
+++ hire_hire/interview/static/js/duel-results.js
@@ -1,4 +1,4 @@
 const counter = document.querySelector('.duel-result__players');
 
-counter.style.gridTemplateColumns = "1fr 500px 1fr";
-counter.style.gap = "32px";
+counter.style.gridTemplateColumns = '1fr 500px 1fr';
+counter.style.gap = '32px';
