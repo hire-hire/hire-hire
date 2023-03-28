3d54f7ec3fd4f532996c1ee79a296ed9656c32f7
Dmitrii Barilkin <dimabaril@gmail.com>
2023-03-28 22:16:43 +0200

    fix mixin

--- hire_hire/add_question/mixins.py
+++ hire_hire/add_question/mixins.py
@@ -1,17 +1,14 @@
-from urllib.parse import urlencode
-
-from django.contrib.admin.views.main import SEARCH_VAR
 from django.http import QueryDict
 
 
 class DefaultFilterMixin:
 
     def changelist_view(self, request, extra_context=None):
-        if request.method == 'GET' and not request.GET:
-            if hasattr(self, 'default_filters'):
-                request.GET = QueryDict(
-                    f'{urlencode(self.default_filters)}&{SEARCH_VAR}=',
-                    encoding=request.encoding,
-                )
-            request.META['QUERY_STRING'] = request.GET.urlencode()
+        if (
+            request.method == 'GET'
+            and not request.GET
+            and hasattr(self, 'default_filters')
+        ):
+            request.GET = QueryDict('', mutable=True)
+            request.GET.update(self.default_filters, q='')
         return super().changelist_view(request, extra_context=extra_context)
