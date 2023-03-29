5c7225add886b0f52031479a2af63e443c9c4e23
Dmitrii Barilkin <dimabaril@gmail.com>
2023-03-29 14:59:10 +0200

    little fix and docstring

--- hire_hire/add_question/mixins.py
+++ hire_hire/add_question/mixins.py
@@ -2,12 +2,22 @@ from django.http import QueryDict
 
 
 class DefaultFilterMixin:
+    '''
+    Default preselected filter.
+    Just take this mixin in your YourClassAdmin and put default_filters field
+    like this:
+    class YourClassAdmin(DefaultFilterMixin, admin.ModelAdmin):
+        default_filters = (('status__exact', 'default_preselected_status'),)
+    .
+    '''
+
+    default_filters = None
 
     def changelist_view(self, request, extra_context=None):
         if (
             request.method == 'GET'
             and not request.GET
-            and hasattr(self, 'default_filters')
+            and self.default_filters
         ):
             request.GET = QueryDict('', mutable=True)
             request.GET.update(self.default_filters, q='')
