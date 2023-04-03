e367032f9c108f1829f80882a41814c0092e851b
juliojm1310 <juliojm1310@gmail.com>
2023-04-03 14:44:50 +0300

    new PATCH updating all feautres

--- hire_hire/api_duel/serializers.py
+++ hire_hire/api_duel/serializers.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from api_interview.serializers import QuestionsSerializer
 from api_duel.services import create_duel
 from duel.models import Duel, DuelPlayer, DuelQuestion
+from duel.services import set_duel_question_is_answered
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -35,6 +36,18 @@ class DuelPlayerSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "name",
+            "good_answers_count",
+        )
+
+
+class DuelUpdateSerializer(serializers.ModelSerializer):
+    players = DuelPlayerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Duel
+        fields = (
+            'wrong_answers_count',
+            'players',
         )
 
 
@@ -75,8 +88,25 @@ class DuelCreateSerializer(serializers.ModelSerializer):
 
 
 class DuelPartialUpdateSerializer(serializers.ModelSerializer):
+    winner_id = serializers.IntegerField()
+    question_id = serializers.IntegerField()
+
     class Meta:
         model = Duel
         fields = (
-            'wrong_answers_count',
+            'winner_id',
+            'question_id',
         )
+
+    def update(self, instance, validated_data):
+        winner_pk = validated_data.get('winner_id')
+        question_id = validated_data.get('question_id')
+        duel = instance
+        DuelPlayer.objects.update_player_and_duel_score(winner_pk=winner_pk, duel=duel)
+        duel_question = duel.questions.get(pk=question_id)
+        set_duel_question_is_answered(duel_question=duel_question)
+        return duel
+
+    def to_representation(self, instance):
+        serializer = DuelUpdateSerializer(instance)
+        return serializer.data
