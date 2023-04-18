fe0ba3aaab13d3c5d54987507f909b740a47510f
Dmitrii Barilkin <dimabaril@gmail.com>
2023-04-18 15:58:27 +0200

    с комментами и принтами

--- hire_hire/api_add_question/serializers.py
+++ hire_hire/api_add_question/serializers.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework import serializers
 
 from add_question.models import AddQuestion
@@ -10,24 +11,33 @@ class AddQuestionSerializer(serializers.ModelSerializer):
         model = AddQuestion
         fields = '__all__'  # ['text', 'answer']
 
-    def get_extra_data(self, obj):
-        print('ser get_extra_data!!!!!!!!!!!!!!!!!!!!')
-        return {
-            'add_questions_for24_count': self.context.get(
-                'add_questions_for24_count'
-            ),
-            'limit_add_questions_per_day': self.context.get(
-                'limit_add_questions_per_day'
-            ),
-        }
-
     def validate(self, data):
-        print('ser validate!!!!!!!!!!!!!!!!!!!!')
-        add_questions_for24_count = self.context.get(
-            'add_questions_for24_count')
-        limit_add_questions_per_day = self.context.get(
-            'limit_add_questions_per_day')
-        if add_questions_for24_count >= limit_add_questions_per_day:
+        print('serializer validate!!!!!!!!!!!!!!!!!!!! если add_questions_for24_count больше 10 то ошибка')
+        # add_questions_for24_count = self.context.get(
+        #     'add_questions_for24_count')
+        # limit_add_questions_per_day = self.context.get(
+        #     'limit_add_questions_per_day')
+        # if add_questions_for24_count >= limit_add_questions_per_day:
+        if AddQuestion.objects.get_24_hours_added_question(
+                    self.context.get('request')
+                ) >= settings.LIMIT_ADD_QUESTIONS_PER_DAY:
             raise serializers.ValidationError(
                 'Вы исчерпали лимит вопросов на день.')
         return data
+
+    def get_extra_data(self, obj):
+        print('serializer get_extra_data!!!!!!!!!!!!!!!!!!!! достаём из контекста add_questions_for24_count')
+        return {
+            # 'add_questions_for24_count': self.context.get(
+            #     'add_questions_for24_count'
+            # ),
+            'add_questions_for24_count': (
+                AddQuestion.objects.get_24_hours_added_question(
+                    self.context.get('request')
+                )
+            ),
+            # 'limit_add_questions_per_day': self.context.get(
+            #     'limit_add_questions_per_day'
+            # ),
+            'limit_add_questions_per_day': settings.LIMIT_ADD_QUESTIONS_PER_DAY,
+        }
