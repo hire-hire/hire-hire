f859fa8500908dbb6f3e1688a71f6fb984328cb3
Dmitrii Barilkin <dimabaril@gmail.com>
2023-04-18 16:50:31 +0200

    first for pull request

--- hire_hire/api_add_question/serializers.py
+++ hire_hire/api_add_question/serializers.py
@@ -9,35 +9,28 @@ class AddQuestionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AddQuestion
-        fields = '__all__'  # ['text', 'answer']
+        fields = '__all__'  # пока так ['text', 'answer']
 
     def validate(self, data):
-        print('serializer validate!!!!!!!!!!!!!!!!!!!! если add_questions_for24_count больше 10 то ошибка')
-        # add_questions_for24_count = self.context.get(
-        #     'add_questions_for24_count')
-        # limit_add_questions_per_day = self.context.get(
-        #     'limit_add_questions_per_day')
-        # if add_questions_for24_count >= limit_add_questions_per_day:
-        if AddQuestion.objects.get_24_hours_added_question(
-                    self.context.get('request')
-                ) >= settings.LIMIT_ADD_QUESTIONS_PER_DAY:
+        if (
+            AddQuestion.objects.get_24_hours_added_question(
+                self.context.get('request')
+            )
+            >= settings.LIMIT_ADD_QUESTIONS_PER_DAY
+        ):
             raise serializers.ValidationError(
-                'Вы исчерпали лимит вопросов на день.')
+                'Вы исчерпали лимит вопросов на день.'
+            )
         return data
 
     def get_extra_data(self, obj):
-        print('serializer get_extra_data!!!!!!!!!!!!!!!!!!!! достаём из контекста add_questions_for24_count')
         return {
-            # 'add_questions_for24_count': self.context.get(
-            #     'add_questions_for24_count'
-            # ),
             'add_questions_for24_count': (
                 AddQuestion.objects.get_24_hours_added_question(
                     self.context.get('request')
                 )
             ),
-            # 'limit_add_questions_per_day': self.context.get(
-            #     'limit_add_questions_per_day'
-            # ),
-            'limit_add_questions_per_day': settings.LIMIT_ADD_QUESTIONS_PER_DAY,
+            'limit_add_questions_per_day': (
+                settings.LIMIT_ADD_QUESTIONS_PER_DAY
+            ),
         }
--- hire_hire/api_add_question/views.py
+++ hire_hire/api_add_question/views.py
@@ -1,43 +1,29 @@
 import uuid
 
-# from django.conf import settings
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 
 from add_question.models import AddQuestion
 from api_add_question.serializers import AddQuestionSerializer
 
 
-class AddQuestionViewSet(viewsets.ModelViewSet):
+class AddQuestionViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = AddQuestion.objects.all()
     serializer_class = AddQuestionSerializer
 
     def dispatch(self, request, *args, **kwargs):
-        print('view dispatch!!!!!!!!!!!!!!!!!!!! здесь суём куки если надо')
         self.user_cookie = request.COOKIES.get('user_cookie')
         response = super().dispatch(request, *args, **kwargs)
         if not self.user_cookie:
             self.user_cookie = uuid.uuid4().hex
-            # response = super().dispatch(request, *args, **kwargs)
             response.set_cookie('user_cookie', self.user_cookie)
-        # else:
-        #     response = super().dispatch(request, *args, **kwargs)
         return response
 
-    # def get_serializer_context(self):
-    #     print('view get_serializer_context!!!!!!!!!!!!!!!!!!!! здесь мы уже сунули add_questions_for24_count до создания вопроса в БД')
-    #     context = super().get_serializer_context()
-    #     print(context)
-    #     context['add_questions_for24_count'] = (
-    #         AddQuestion.objects.get_24_hours_added_question(self.request)
-    #     )
-    #     context[
-    #         'limit_add_questions_per_day'
-    #     ] = settings.LIMIT_ADD_QUESTIONS_PER_DAY
-    #     # print(context)
-    #     return context
-
     def perform_create(self, serializer):
-        print('view perform_create!!!!!!!!!!!!!!!!!!!!! сохраняем вопрос в БД')
         instance = serializer.save()
         instance.ip_address = self.request.META.get('REMOTE_ADDR')
         if self.request.user.is_authenticated:
