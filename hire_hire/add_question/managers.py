63f5961b7a3669eae8380aa270e94366fd708e2f
Badsnus <anton.098.bal@gmail.com>
2023-04-24 15:42:49 +0500

    мне эни вей эта функция не нравится, это очень странно, что мы должны передавать и юзера и куки

--- hire_hire/add_question/managers.py
+++ hire_hire/add_question/managers.py
@@ -3,15 +3,13 @@ from django.utils import timezone
 
 
 class AddQuestionManager(models.Manager):
-    """Возвращает количество предложенных вопросов за 24 часа."""
-    def get_24_hours_added_question(self, user, user_cookie):
+    def get_24_hours_added_question_count(self, user, user_cookie):
         ago_24_hours = timezone.now() - timezone.timedelta(hours=24)
-        if user.is_authenticated:
-            return self.get_queryset().filter(
-                pub_date__gte=ago_24_hours,
-                author=user,
-            ).count()
+        user_data_dict = (
+            dict(author=user) if user.is_authenticated
+            else dict(user_cookie=user_cookie)
+        )
         return self.get_queryset().filter(
             pub_date__gte=ago_24_hours,
-            user_cookie=user_cookie,
+            **user_data_dict,
         ).count()
