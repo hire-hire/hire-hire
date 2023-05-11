3a96cce6b2781090d39804b1f8d3d3dfca8d8e55
Кривошеев Никита <sharkbreeder@yandex.ru>
2023-05-11 13:16:41 +0300

    STYLE

--- hire_hire/hire_hire/urls.py
+++ hire_hire/hire_hire/urls.py
@@ -27,7 +27,11 @@ if settings.DEBUG:
         document_root=settings.MEDIA_ROOT,
     )
     urlpatterns += [
-        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path(
+            'api/schema/',
+            SpectacularAPIView.as_view(),
+            name='schema'
+        ),
         path(
             'api/schema/swagger-ui/',
             SpectacularSwaggerView.as_view(url_name='schema'),
