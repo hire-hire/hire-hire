0e71f365c2b347d22dca1e0c119272d52cde8f6b
Dmitrii Barilkin <dimabaril@gmail.com>
2023-05-18 19:54:31 +0200

    get_24_hours_added_question_count to by_user and by_user_cookie_id

--- hire_hire/add_question/managers.py
+++ hire_hire/add_question/managers.py
@@ -1,17 +1,36 @@
 from django.db import models
 from django.utils import timezone
 
-from add_question.services import get_user_data_dict
-
 
 class AddQuestionManager(models.Manager):
+    ago_24_hours = timezone.now() - timezone.timedelta(hours=24)
+
     def get_24_hours_added_question_count(self, user, user_cookie_id):
-        ago_24_hours = timezone.now() - timezone.timedelta(hours=24)
+        if user.is_authenticated:
+            return self.get_24_hours_added_question_count_by_user(user)
+        return self.get_24_hours_added_question_count_by_user_cookie_id(
+            user_cookie_id,
+        )
+
+    def get_24_hours_added_question_count_by_user(self, user):
         return (
             self.get_queryset()
             .filter(
-                pub_date__gte=ago_24_hours,
-                **get_user_data_dict(user, user_cookie_id),
+                pub_date__gte=self.ago_24_hours,
+                author=user,
+            )
+            .count()
+        )
+
+    def get_24_hours_added_question_count_by_user_cookie_id(
+        self,
+        user_cookie_id,
+    ):
+        return (
+            self.get_queryset()
+            .filter(
+                pub_date__gte=self.ago_24_hours,
+                user_cookie_id=user_cookie_id,
             )
             .count()
         )
