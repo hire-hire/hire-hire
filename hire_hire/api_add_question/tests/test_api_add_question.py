9be208adbebb0bef55bef1b239bd4b4c3152eaa5
Dmitrii Barilkin <dimabaril@gmail.com>
2023-05-25 12:11:46 +0200

    flake8 & comments

--- hire_hire/api_add_question/tests/test_api_add_question.py
+++ hire_hire/api_add_question/tests/test_api_add_question.py
@@ -41,12 +41,12 @@ def api_client_unauth_user():
 @pytest.fixture
 def api_client_auth_user(django_user_model):
     auth_user = django_user_model.objects.create(
-        username="some_user",
-        password="Some_password_09876",
+        username='some_user',
+        password='Some_password_09876',
     )
     token = str(AccessToken.for_user(auth_user))
     client = APIClient()
-    auth_header_type = settings.SIMPLE_JWT["AUTH_HEADER_TYPES"][0]
+    auth_header_type = settings.SIMPLE_JWT['AUTH_HEADER_TYPES'][0]
     client.credentials(HTTP_AUTHORIZATION=auth_header_type + ' ' + token)
     return client
 
@@ -68,22 +68,20 @@ def test_add_question_unauth(
     api_client_unauth_user, language_1, language_2, language_3
 ):
     """Проверка добавления вопроса анонимом."""
-    # url = '/api/v1/add_question/'
     url = reverse('api:api_add_question:add_question-list')
 
     assert AddQuestion.objects.count() == 0, 'В базе уже есть вопросы!!!'
 
     for i in range(1, settings.LIMIT_ADD_QUESTIONS_PER_DAY + 1):
         data = {
-            "text": f"Вопрос номер {i}?",
-            "answer": f"Ответ номер {i}",
-            "language": 2,
+            'text': f'Вопрос номер {i}?',
+            'answer': f'Ответ номер {i}',
+            'language': 2,
         }
         response = api_client_unauth_user.post(
             path=url,
             data=data,
         )
-        # print(response.data)
         assert response.status_code == 201, 'Ошибка при добавлении вопроса!!!'
         assert AddQuestion.objects.count() == i, 'Вопрос не добавился!!!'
         assert AddQuestion.objects.last().text == f'Вопрос номер {i}?'
@@ -96,9 +94,9 @@ def test_add_question_unauth(
     assert AddQuestion.objects.count() == settings.LIMIT_ADD_QUESTIONS_PER_DAY
 
     data = {
-        "text": "Вопрос на котором лимит исчерпан?",
-        "answer": "Ответ на котором лимит исчерпан",
-        "language": 2,
+        'text': 'Вопрос на котором лимит исчерпан?',
+        'answer': 'Ответ на котором лимит исчерпан',
+        'language': 2,
     }
     response = api_client_unauth_user.post(
         path=url,
@@ -118,15 +116,14 @@ def test_add_question_auth(
 
     for i in range(1, settings.LIMIT_ADD_QUESTIONS_PER_DAY + 1):
         data = {
-            "text": f"Вопрос номер {i}?",
-            "answer": f"Ответ номер {i}",
-            "language": 2,
+            'text': f'Вопрос номер {i}?',
+            'answer': f'Ответ номер {i}',
+            'language': 2,
         }
         response = api_client_auth_user.post(
             path=url,
             data=data,
         )
-        # print(response.data)
         assert response.status_code == 201, 'Ошибка при добавлении вопроса!!!'
         assert AddQuestion.objects.count() == i, 'Вопрос не добавился!!!'
         assert AddQuestion.objects.last().text == f'Вопрос номер {i}?'
@@ -139,9 +136,9 @@ def test_add_question_auth(
     assert AddQuestion.objects.count() == settings.LIMIT_ADD_QUESTIONS_PER_DAY
 
     data = {
-        "text": "Вопрос на котором лимит исчерпан?",
-        "answer": "Ответ на котором лимит исчерпан",
-        "language": 2,
+        'text': 'Вопрос на котором лимит исчерпан?',
+        'answer': 'Ответ на котором лимит исчерпан',
+        'language': 2,
     }
     response = api_client_auth_user.post(
         path=url,
