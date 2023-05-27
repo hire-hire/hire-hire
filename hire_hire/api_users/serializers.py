049804580d9a3b09f62bbd9cf278ebc2ac882e89
Кривошеев Никита <sharkbreeder@yandex.ru>
2023-05-27 12:50:32 +0300

    flag added to api

---
+++ hire_hire/api_users/serializers.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import get_user_model
+from djoser.serializers import UserSerializer
+
+
+User = get_user_model()
+
+
+class CustomUserSerializer(UserSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'is_duel_moderator')
