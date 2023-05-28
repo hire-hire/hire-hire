8c0f6b4ec58a3915bc08795e8c9999daac78a917
Юлия Орлова <zaikunets@yandex.ru>
2023-05-28 12:27:48 +0300

    Тесты api_contributors ревью

--- hire_hire/api_contributors/tests/test_contributors.py
+++ hire_hire/api_contributors/tests/test_contributors.py
@@ -1,7 +1,6 @@
 import pytest
 
 from api_contributors.serializers import ContributorSerializer
-from contributors.models import Contributor
 
 
 class TestContributorsApi:
@@ -13,19 +12,6 @@ class TestContributorsApi:
     def test_get_contributors(self, client):
         response = client.get(self.url_contributors)
         assert response.status_code == 200
-
-    @pytest.mark.django_db
-    def test_get_contributors_fields(self, client, contributor):
-        response = client.get(self.url_contributors)
-        assert response.status_code == 200
-        assert len(response.json()) != 0
-        test_contributor = response.json()[0]
-        for field in Contributor._meta.fields:
-            if field.name != 'id':
-                assert field.name in test_contributor, (
-                    f'Нет поля {field.name} '
-                    f'в ответе {self.url_contributors}'
-                )
 
     @pytest.mark.django_db
     def test_contributors_answer_type(self, client, contributor):
@@ -41,8 +27,9 @@ class TestContributorsApi:
         assert response.status_code == 200
         contributors = response.json()
         assert len(contributors) == 1
-        assert 'thumbnail_image' in contributors[0]
-        assert (contributors[0]['thumbnail_image'] ==
+        test_contributor = contributors[0]
+        assert 'thumbnail_image' in test_contributor
+        assert (test_contributor['thumbnail_image'] ==
                contributor.thumbnail_image)
 
     @pytest.mark.django_db
@@ -50,14 +37,15 @@ class TestContributorsApi:
             self, client, contributor, contributor_contact1):
         response = client.get(self.url_contributors)
         assert response.status_code == 200
-        assert len(response.json()) != 0
-        contributor = response.json()[0]
-        assert 'contacts' in contributor
-        contributor_contacts = contributor.get('contacts')[0]
-        assert isinstance(contributor.get('contacts'), list)
-        assert isinstance(contributor_contacts, dict)
-        assert 'social_network' in contributor_contacts
-        assert 'contact' in contributor_contacts
+        response = response.json()
+        assert len(response) != 0
+        contributor = response[0]
+        if 'contacts' in contributor:
+            contributor_contacts = contributor.get('contacts')[0]
+            assert isinstance(contributor.get('contacts'), list)
+            assert isinstance(contributor_contacts, dict)
+            assert 'social_network' in contributor_contacts
+            assert 'contact' in contributor_contacts
 
     @pytest.mark.django_db
     def test_contributors_contacts_fields(
@@ -65,20 +53,23 @@ class TestContributorsApi:
             contributor_contact1, contributor_contact2):
         response = client.get(self.url_contributors)
         assert response.status_code == 200
-        assert len(response.json()) != 0
-        contributor_contacts = response.json()[0].get('contacts')
+        response = response.json()
+        assert len(response) != 0
+        contributor_contacts = response[0].get('contacts')
         assert len(contributor_contacts) == 2
-        assert contributor_contacts[0].get('social_network') == (
+        test_contact = contributor_contacts[0]
+        assert test_contact.get('social_network') == (
             contributor_contact1.social_network)
-        assert contributor_contacts[0].get('contact') == (
+        assert test_contact.get('contact') == (
             contributor_contact1.contact)
 
     @pytest.mark.django_db
     def test_get_contributor_serializer_fields(self, client, contributor):
         response = client.get(self.url_contributors)
         assert response.status_code == 200
-        assert len(response.json()) != 0
-        test_contributor = response.json()[0]
+        response = response.json()
+        assert len(response) != 0
+        test_contributor = response[0]
         for field in ContributorSerializer.Meta.fields:
             if field != 'id':
                 assert field in test_contributor, (
@@ -87,7 +78,7 @@ class TestContributorsApi:
                 )
 
     @pytest.mark.django_db
-    def test_post_contributors(self, client, admin_user):
+    def test_post_auth_user_not_allowed(self, client, admin_user):
         client.force_login(admin_user)
         response = client.post(
             self.url_contributors,
@@ -96,7 +87,22 @@ class TestContributorsApi:
                 'last_name': 'Б',
                 'middle_name': 'Nothing',
                 'role': 'разработчик',
-                'photo': 'fixtures/image_for_tests.png',
+                'photo': 'team/image_for_tests.png',
+            },
+        )
+        assert response.status_code == 405
+
+    @pytest.mark.django_db
+    def test_put_auth_user_not_allowed(self, client, admin_user, contributor):
+        client.force_login(admin_user)
+        response = client.put(
+            f'{self.url_contributors}{contributor.id}/',
+            data={
+                'first_name': 'Новое имя',
+                'last_name': 'Б',
+                'middle_name': 'Nothing',
+                'role': 'разработчик',
+                'photo': 'team/image_for_tests.png',
             },
         )
         assert response.status_code == 405
