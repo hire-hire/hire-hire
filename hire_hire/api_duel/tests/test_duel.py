7c623dcf132baaffdb2b7d745a665a16361ce20e
juliojm1310 <juliojm1310@gmail.com>
2023-06-02 13:39:23 +0300

    Add tests for PATCH method

--- hire_hire/api_duel/tests/test_duel.py
+++ hire_hire/api_duel/tests/test_duel.py
@@ -147,7 +147,8 @@ class TestDuelApi:
 
     @pytest.mark.django_db(transaction=True)
     def test_get_invalid_duel(self, moderator_client, duel_instance):
-        resp = moderator_client.get(f'{self.url_duel}{99999999999}/')
+        invalid_duel_pk = 99999999999  # This duel does not exist
+        resp = moderator_client.get(f'{self.url_duel}{invalid_duel_pk}/')
         assert (
                 resp.status_code == 404
         ), 'Ответ при несуществующем объекте приходит не со статусом 404'
@@ -168,3 +169,155 @@ class TestDuelApi:
                 len(resp.json().get('players'))
                 == duel_instance.players.count()
         ), 'Кол-во игроков не совпадает с кол-вом в базе'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_patch_duel_not_auth(self, client, duel_instance):
+        resp = client.patch(f'{self.url_duel}{duel_instance.id}/')
+        assert (
+                resp.status_code == 401
+        ), 'Ответ неавторизованному приходит не со статусом 401'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_patch_duel_not_owner(self, user_client, duel_instance):
+        resp = user_client.patch(f'{self.url_duel}{duel_instance.id}/')
+        assert (
+                resp.status_code == 403
+        ), 'Ответ авторизованному невладелецу приходит не со статусом 403'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_duel_update(self, moderator_client, duel_instance):
+        player_ids = duel_instance.players.values_list('id', flat=True)
+        question_ids = duel_instance.questions.values_list('id', flat=True)
+
+        data = {
+            'winner_id': player_ids[0],
+            'question_id': question_ids[0],
+        }
+
+        resp = moderator_client.patch(
+            f'{self.url_duel}{duel_instance.id}/',
+            data=data,
+            format='json',
+        )
+        assert (
+                resp.status_code == 200
+        ), 'Ответ после обновления приходит не со статусом 200'
+
+        # Check that the duel instance was correctly updated
+        assert (
+                duel_instance.players.get(
+                    id=data['winner_id']).good_answers_count == 1
+        ), 'Счет победителя не был увеличен'
+        assert (
+            duel_instance.questions.get(id=data['question_id']).is_answered
+        ), 'Поле is_answered вопроса не установлено в True'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_duel_update_invalid_winner(self, moderator_client, duel_instance):
+        question_ids = duel_instance.questions.values_list('id', flat=True)
+
+        invalid_data = {
+            'winner_id': 123456789,  # This player does not exist
+            'question_id': question_ids[0],
+        }
+
+        resp = moderator_client.patch(
+            f'{self.url_duel}{duel_instance.id}/',
+            data=invalid_data,
+            format='json',
+        )
+        assert (
+                resp.status_code == 404
+        ), ('Ответ при указании несуществующего игрока '
+            'не приходит со статусом 404')
+
+    @pytest.mark.django_db(transaction=True)
+    def test_duel_update_invalid_question(
+            self,
+            moderator_client,
+            duel_instance,
+    ):
+        player_ids = duel_instance.players.values_list(
+            'id',
+            flat=True,
+        )
+
+        invalid_data = {
+            'winner_id': player_ids[0],
+            'question_id': 123456789,  # This question does not exist
+        }
+
+        resp = moderator_client.patch(
+            f'{self.url_duel}{duel_instance.id}/',
+            data=invalid_data,
+            format='json',
+        )
+        assert (
+                resp.status_code == 404
+        ), ('Ответ при указании несуществующего '
+            'вопроса не приходит со статусом 404')
+
+    @pytest.mark.django_db(transaction=True)
+    def test_duel_update_no_winner(self, moderator_client, duel_instance):
+        question_ids = duel_instance.questions.values_list('id', flat=True)
+        wrong_answers_count_initial = duel_instance.wrong_answers_count
+
+        data = {
+            'winner_id': -1,  # No winner
+            'question_id': question_ids[0],
+        }
+
+        resp = moderator_client.patch(
+            f'{self.url_duel}{duel_instance.id}/',
+            data=data,
+            format='json',
+        )
+        assert (
+                resp.status_code == 200
+        ), 'Ответ после обновления приходит не со статусом 200'
+
+        duel_instance.refresh_from_db()
+
+        assert (
+                duel_instance.wrong_answers_count
+                == wrong_answers_count_initial + 1
+        ), 'Счетчик неправильных ответов не увеличен'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_duel_update_question_already_answered(
+            self,
+            moderator_client,
+            duel_instance,
+    ):
+        player_ids = duel_instance.players.values_list('id', flat=True)
+        question_ids = duel_instance.questions.values_list('id', flat=True)
+
+        # First update - should be successful
+        data = {
+            'winner_id': player_ids[0],
+            'question_id': question_ids[0],
+        }
+
+        resp = moderator_client.patch(
+            f'{self.url_duel}{duel_instance.id}/',
+            data=data,
+            format='json',
+        )
+        assert (
+                resp.status_code == 200
+        ), 'Ответ после обновления приходит не со статусом 200'
+
+        # Second update - should fail because the question was already answered
+        resp = moderator_client.patch(
+            f'{self.url_duel}{duel_instance.id}/',
+            data=data,
+            format='json',
+        )
+        assert (
+                resp.status_code == 400
+        ), ('Ответ на обновление уже ответившего вопроса '
+            'не имел кода статуса 400')
+        assert (
+                resp.json().get('detail') == 'Question is already answered!'
+        ), ('Сообщение об ошибке при обновлении '
+            'уже ответившего вопроса неверно')
