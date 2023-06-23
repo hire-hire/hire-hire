949ba748bfca0c4dc287c1cce9338bb65aa20db6
Dmitrii Barilkin <dimabaril@gmail.com>
2023-06-23 22:16:53 +0200

    tests 2

--- hire_hire/api_add_question/urls.py
+++ hire_hire/api_add_question/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api_add_question.views import (
-    AddQuestionViewSet,
     AddedQestionsAndLimitView,
+    AddQuestionViewSet,
 )
 
 app_name = 'api_add_question'
