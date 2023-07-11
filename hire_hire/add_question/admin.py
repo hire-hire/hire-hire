bac38d9ea66b40f14975217e5676d33a446c1dce
Dmitrii Barilkin <dimabaril@gmail.com>
2023-07-11 13:06:15 +0200

    Im not sure

--- hire_hire/add_question/admin.py
+++ hire_hire/add_question/admin.py
@@ -1,7 +1,9 @@
 from django import forms
-from django.contrib import admin
-from django.db import models
+from django.contrib import admin, messages
+from django.contrib.admin.utils import model_ngettext
+from django.db import models, router, transaction
 from django.utils.safestring import mark_safe
+from django.utils.translation import ngettext
 
 from add_question.mixins import DefaultFilterMixin
 from add_question.models import AddQuestion
@@ -12,6 +14,8 @@ from interview.models import Question
 @admin.register(AddQuestion)
 class AddQuestionAdmin(DefaultFilterMixin, admin.ModelAdmin):
     """Админ панель предложенных вопросов."""
+
+    list_per_page = 8
 
     APPROVE = '_approve'
     REJECT = '_reject'
@@ -34,23 +38,22 @@ class AddQuestionAdmin(DefaultFilterMixin, admin.ModelAdmin):
 
     formfield_overrides = {
         models.TextField: {
-            'widget': forms.Textarea(attrs={'rows': 4, 'cols': 15}),
+            'widget': forms.Textarea(attrs={'rows': 3, 'cols': 45}),
         },
     }
 
     list_display = (
         AddQuestion.id.field.name,
         AddQuestion.status.field.name,
-        AddQuestion.language.field.name,
         AddQuestion.text.field.name,
         AddQuestion.answer.field.name,
-        AddQuestion.ip_address.field.name,
         AddQuestion.pub_date.field.name,
-        AddQuestion.author.field.name,
+        AddQuestion.language.field.name,
     )
     list_editable = (
         AddQuestion.text.field.name,
         AddQuestion.answer.field.name,
+        AddQuestion.language.field.name,
     )
     search_fields = (
         AddQuestion.language.field.name,
@@ -115,3 +118,56 @@ class AddQuestionAdmin(DefaultFilterMixin, admin.ModelAdmin):
             obj.save()
             self.message_user(request, 'Вопрос отклонён.')
         return super().response_change(request, obj)
+
+    def response_action(self, request, queryset):
+        """
+        When we use an our custom action and we have list_editable fields this
+        part saves all the changes that have been made in list_editable fields.
+        That part of code is exactly part that we need from changelist_view
+        method.
+        """
+
+        print(self.get_changelist_instance(request).__dict__)
+
+        FormSet = self.get_changelist_formset(request)
+        modified_objects = self._get_list_editable_queryset(
+            request,
+            FormSet.get_default_prefix(),
+        )
+        formset = FormSet(
+            request.POST,
+            request.FILES,
+            queryset=modified_objects,
+        )
+        if formset.is_valid():
+            changecount = 0
+            with transaction.atomic(using=router.db_for_write(self.model)):
+                for form in formset.forms:
+                    if form.has_changed():
+                        obj = self.save_form(request, form, change=True)
+                        self.save_model(request, obj, form, change=True)
+                        self.save_related(
+                            request,
+                            form,
+                            formsets=[],
+                            change=True,
+                        )
+                        change_msg = self.construct_change_message(
+                            request,
+                            form,
+                            None,
+                        )
+                        self.log_change(request, obj, change_msg)
+                        changecount += 1
+            if changecount:
+                msg = ngettext(
+                    "%(count)s %(name)s was changed successfully.",
+                    "%(count)s %(name)s were changed successfully.",
+                    changecount,
+                ) % {
+                    "count": changecount,
+                    "name": model_ngettext(self.opts, changecount),
+                }
+                self.message_user(request, msg, messages.SUCCESS)
+
+        return super().response_action(request, queryset)
