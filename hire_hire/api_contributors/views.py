38fc13ab397b467b3a9a2155be56322a1b479a0e
Юлия Орлова <zaikunets@yandex.ru>
2023-07-30 23:09:02 +0300

    changing parents

--- hire_hire/api_contributors/views.py
+++ hire_hire/api_contributors/views.py
@@ -1,13 +1,11 @@
-from rest_framework import viewsets
-from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
 from api_contributors.serializers import ContributorSerializer
 from contributors.models import Contributor
 
 
-class ContributorsListViewSet(viewsets.ViewSet):
-    def list(self, request):
-        queryset = (Contributor.objects.
-                    get_contributors_with_contacts_and_roles())
-        serializer = ContributorSerializer(queryset, many=True)
-        return Response(serializer.data)
+class ContributorsListViewSet(mixins.ListModelMixin, GenericViewSet):
+    queryset = (Contributor.objects.
+                get_contributors_with_contacts_and_roles())
+    serializer_class = ContributorSerializer
