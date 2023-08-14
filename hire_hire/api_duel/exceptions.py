bd7f7a4debca940c27af1508f3ac9b21793ebdfd
juliojm1310 <juliojm1310@gmail.com>
2023-08-14 10:50:00 +0300

    Add API exceptions for PATCH api_duel bad requests

--- hire_hire/api_duel/exceptions.py
+++ hire_hire/api_duel/exceptions.py
@@ -6,3 +6,15 @@ class QuestionAlreadyAnswered(APIException):
     status_code = 400
     default_detail = _('Question is already answered!')
     default_code = 'Bad Request'
+
+
+class DuelPlayerDoesNotExist(APIException):
+    status_code = 400
+    default_detail = _('Player does not exists for this duel!')
+    default_code = 'Bad Request'
+
+
+class DuelQuestionDoesNotExist(APIException):
+    status_code = 400
+    default_detail = _('Question does not exists for this duel!')
+    default_code = 'Bad Request'
