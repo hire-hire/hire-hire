7f7a5fd00dc6f8c628a005d5636873f71041e196
Badsnus <anton.098.bal@gmail.com>
2023-08-18 11:56:20 +0500

    minor fix

--- hire_hire/hire_hire/settings.py
+++ hire_hire/hire_hire/settings.py
@@ -243,26 +243,3 @@ DONATION = Donation(
     ),
     os.getenv('YOKASSA_URL', default='https://api.yookassa.ru/v3/payments'),
 )
-
-
-LOGGING = {
-    'version': 1,
-    'filters': {
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        }
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        }
-    }
-}
