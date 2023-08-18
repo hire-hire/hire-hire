8639381144e72b1e38ab9176ab74180923306fd9
Badsnus <anton.098.bal@gmail.com>
2023-08-18 14:57:30 +0500

    replace args -> kwargs

--- hire_hire/api_interview/services.py
+++ hire_hire/api_interview/services.py
@@ -5,7 +5,7 @@ def create_interview(ser_validated_data):
     question_count = ser_validated_data.pop('question_count')
     instance = Interview.objects.create(**ser_validated_data)
     questions = Question.objects.get_random_questions(
-        question_count,
+        cnt=question_count,
         user=ser_validated_data['user'],
     )
     instance.questions.add(*questions)
