9e861ae0c107ad7f981bf36706a8f5306d3b33cc
Nik <sharkbreeder@yandex.ru>
2023-10-27 17:40:06 +0300

    refactor: removed custom middleware

--- hire_hire/hire_hire/settings.py
+++ hire_hire/hire_hire/settings.py
@@ -56,8 +56,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-
-    'api.logging_middleware.CatchErrorsMiddleware',
 ]
 
 ROOT_URLCONF = 'hire_hire.urls'
